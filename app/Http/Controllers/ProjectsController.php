778ae35d2475efa2de13a29b6fda3d09713632b1
Tiago Estevao <tiago.estevao.o@gmail.com>
2020-08-05 08:02:52 -0300

    Alterado controller projetos para criar  tasks ao criar projetos

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -52,6 +52,16 @@ class ProjectsController extends Controller
         
         $project = auth()->user()->projects()->create($this->validateForms());
         
+        if($request->has('tasks')){
+            foreach($request->tasks as $task){
+                $project->addTask($task['body']);
+            }
+        }
+
+        if($request->wantsJson()){
+            return ['message' => $project->path()];
+        }
+
         return redirect($project ->path());
     }
 
